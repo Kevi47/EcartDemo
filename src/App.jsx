e36f6340b668fa76cf84955143ae1b64db6cc14d
Kevin <127479345+Kevi47@users.noreply.github.com>
2023-04-19 18:01:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { Navbar,Banner } from "./Components/Combonents";
+import { Navbar} from "./Components/Combonents";
 
 import "./App.scss";
 function App() {
   return (
     <div className="App">
       <Navbar />
-    <Banner/>
     </div>
   );
 }
